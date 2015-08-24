b3324e196b5b6b13a6c321c8a45b0c3f923abaef
Styx- <morganm5201@gmail.com>
2015-08-24 11:07:53 -0400

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get			'login'		=>	'sessions#new'
   post		'login'		=>	'sessions#create'
   delete	'logout'	=>	'sessions#destroy'
-
   resources :users
+  resources :account_activations, only: [:edit]
 end
